c609e826e66ae356427092e304c5b2c6970e4fa1
Owen Trueblood <jmptable@mit.edu>
2017-04-23 17:19:10 -0400

    Fix style

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,48 @@
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = [
-    {
-        name: 'client-side',
-        entry: './client/src/main.js',
-        output: {
-            path: __dirname,
-            filename: 'client/build/app.js',
-        },
-        module: {
-            loaders: [
-                {
-                    test: /\.js$/,
-                    exclude: /(node_modules)/,
-                    loader: 'babel-loader',
-                    query: {
-                        plugins: [],
-                        presets: ['es2015'],
-                    },
-                },
-            ],
-        },
+  {
+    name: 'client-side',
+    entry: './client/src/main.js',
+    output: {
+      path: __dirname,
+      filename: 'client/build/app.js',
     },
-    {
-        name: 'server-side',
-        target: 'node',
-        externals: [nodeExternals()],
-        entry: './server/src/main.js',
-        output: {
-            path: __dirname,
-            filename: './server/build/server.js',
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          loader: 'babel-loader',
+          query: {
+            plugins: [],
+            presets: ['es2015'],
+          },
         },
-        module: {
-            loaders: [
-                {
-                    test: /\.js$/,
-                    exclude: /(node_modules)/,
-                    loader: 'babel-loader',
-                    query: {
-                        plugins: [],
-                        presets: ['es2015'],
-                    },
-                },
-            ],
-        },
+      ],
     },
+  },
+  {
+    name: 'server-side',
+    target: 'node',
+    externals: [nodeExternals()],
+    entry: './server/src/main.js',
+    output: {
+      path: __dirname,
+      filename: './server/build/server.js',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          loader: 'babel-loader',
+          query: {
+            plugins: [],
+            presets: ['es2015'],
+          },
+        },
+      ],
+    },
+  },
 ];
